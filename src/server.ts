800039189f0490fb057ae0941c6ae716b995c0f0
Nicolas Decat <nicolas.decat@gmail.com>
2021-11-07 18:48:58 +0100

    add cors

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,17 @@ import config from 'config';
 import routes from './routes/routes';
 import deserializeUser from './middlewares/deserializeUser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
-const PORT = process.env.PORT || config.get<number>('config.port');
+const PORT = process.env.port || config.get<number>('config.port');
 const app: Application = express();
 
+app.use(
+  cors({
+    origin: process.env.origin || config.get<string>('config.origin'),
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(deserializeUser);
